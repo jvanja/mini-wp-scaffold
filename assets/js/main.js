54f3207748e7f2218c47bbf4dd0ca77df5ca4054
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-04-13 12:29:35 +0200

    reformated; fixed indent_style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
-import '../scss/style.scss';
+import "../scss/style.scss";
 
-console.log('Hello from main.js');
+console.log("Hello from main.js");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Hello, HMR is working!';
-    document.body.appendChild(h1);
+document.addEventListener("DOMContentLoaded", () => {
+  const h1 = document.createElement("h1");
+  h1.textContent = "Hello, HMR is working!";
+  document.body.appendChild(h1);
 
-    if (import.meta.hot) {
-        import.meta.hot.accept(() => {
-            console.log('HMR accepted!');
-            h1.textContent = 'HMR updated the content!';
-        });
-    }
+  if (import.meta.hot) {
+    import.meta.hot.accept(() => {
+      console.log("HMR accepted!");
+      h1.textContent = "HMR updated the content!";
+    });
+  }
 });
